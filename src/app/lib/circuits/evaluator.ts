9836f3703e5136de5f78af279823cf3147877ffe
myname <e@mail.com>
2025-05-16 12:08:47 +0200

    corrected the evaluator to follow the new specs

--- src/app/lib/circuits/evaluator.ts
+++ src/app/lib/circuits/evaluator.ts
@@ -1,82 +1,90 @@
 import { sha256Compression } from "./components/sha256";
 import { encryptBlock, decryptBlock } from "./components/aes-ctr";
-import { bytesToHex, verifyAuthorization } from "viem/utils";
-import { bytesArraysAreEqual } from "../helpers";
+import { binAdd, binMult, binEquals } from "./components/simpleOps";
 
 export type Gate = [number, number[]]; // [operationId, sons]
 export type Circuit = Gate[];
-type Instruction = (key: Uint8Array, data: Uint8Array[]) => Promise<Uint8Array>;
+type EvaluatedGate = Uint8Array;
+type EvaluatedCircuit = EvaluatedGate[];
+type Instruction = (data: Uint8Array[]) => Promise<Uint8Array>;
 
 const VERSIONS_INSTRUCTIONS: Instruction[][] = [
-    /* version 0 */ [sha256Compression, encryptBlock, decryptBlock, equals],
+    /* version 0 */ [
+        sha256Compression,
+        encryptBlock,
+        decryptBlock,
+        binAdd,
+        binMult,
+        binEquals,
+    ],
 ];
 
-async function equals(
-    key: Uint8Array,
-    data: Uint8Array[]
-): Promise<Uint8Array> {
-    return new Uint8Array([bytesArraysAreEqual(data[0], data[1]) ? 1 : 0]);
-}
-
-function getGateSons(
+// Retrieves the value of the gate's sons after they are evaluated
+function getEvaluatedSons(
     gate: Gate,
     evaluatedCircuit: Uint8Array[],
-    constants: Uint8Array[]
+    constants?: Uint8Array[]
 ): Uint8Array[] {
     let sons = [];
 
     for (let s of gate[1]) {
-        if (s >= 0) sons.push(evaluatedCircuit[s]);
-        else sons.push(constants[-(s + 1)]);
+        if (s >= 0) {
+            if (s >= evaluatedCircuit.length)
+                throw new Error(
+                    "One of the sons' index is not smaller than the gate's"
+                );
+            sons.push(evaluatedCircuit[s]);
+        } else if (constants) sons.push(constants[-(s + 1)]);
+        else throw new Error(`Missing constant with id no ${s}`);
     }
 
     return sons;
 }
 
+/**
+ * Evaluates the provided circuit using the provided input and constants,
+ * following the version of the instruction set
+ *
+ * @param {Uint8Array[]} inputBlocks Input to the circuit split into blocks. It
+ * must have as many elements as the number of dummy gates in the circuit
+ * @param {Circuit} circuit Circuit to evaluate
+ * @param {number} version Version of the instruction set
+ * @param {Uint8Array[]?} constants Constants used for the evaluation
+ * @returns {Promise<[EvaluatedGate, EvaluatedCircuit?]>} The value of the last
+ * gate (root) and the entire circuit after evaluation
+ */
 export async function evaluateCircuit(
-    input: Uint8Array,
-    inputBlockSize: number,
+    inputBlocks: Uint8Array[],
     circuit: Circuit,
-    key: Uint8Array,
-    description: Uint8Array,
-    version: number
-): Promise<Uint8Array> {
+    version: number,
+    constants?: Uint8Array[]
+): Promise<[EvaluatedGate, EvaluatedCircuit?]> {
+    if (inputBlocks.length == 0) return [new Uint8Array(), undefined];
+
     if (!(0 <= version && version < VERSIONS_INSTRUCTIONS.length))
-        throw Error("invalid version");
-    if (inputBlockSize < 0 || (inputBlockSize == 0 && input.length > 0))
-        throw Error("Invalid block size");
+        throw new Error("Invalid version");
 
-    // if the input is empty (input.length == 0), we say that the circuit has a
-    // single gate for the empty input and then the operations
-    const numBlocks =
-        input.length > 0 ? Math.ceil(input.length / inputBlockSize) : 1;
-    const instructions = VERSIONS_INSTRUCTIONS[version];
-
-    if (circuit[numBlocks - 1][0] >= 0 || circuit[numBlocks][0] < 0)
-        throw Error(
-            "Block size and input size don't match the circuit's dummy gates"
-        );
+    if (inputBlocks.length > circuit.length)
+        throw new Error("Input has too many blocks");
 
     let evaluatedCircuit = [];
     // Adding the base elements
-    for (let i = 0; i < numBlocks - 1; ++i) {
-        const nextBlock = input.slice(
-            inputBlockSize * i,
-            inputBlockSize * (i + 1)
-        );
-        evaluatedCircuit.push(nextBlock);
+    for (let i = 0; i < inputBlocks.length - 1; ++i) {
+        if (circuit[i][0] != -1)
+            throw Error("Input doesn't fit in dummy gates");
+        evaluatedCircuit.push(inputBlocks[i]);
     }
 
-    const lastBlockIdx =
-        input.length > 0 ? inputBlockSize * (numBlocks - 1) : 0;
-    evaluatedCircuit.push(input.slice(lastBlockIdx)); // last block
-
-    for (const gate of circuit.slice(numBlocks)) {
-        const sons = getGateSons(gate, evaluatedCircuit, [description]);
+    const instructions = VERSIONS_INSTRUCTIONS[version];
+    for (const gate of circuit.slice(inputBlocks.length)) {
+        const sons = getEvaluatedSons(gate, evaluatedCircuit, constants);
 
         const op = instructions[gate[0]];
-        const result = await op(key, sons);
-        evaluatedCircuit.push();
+        if (!op)
+            throw new Error(`Invalid opcode ${gate[0]} for version ${version}`);
+
+        const result = await op(sons);
+        evaluatedCircuit.push(result);
     }
 
     if (evaluatedCircuit.length != circuit.length)
@@ -84,5 +92,5 @@ export async function evaluateCircuit(
             "something wrong happened during the circuit's evaluation"
         );
 
-    return evaluatedCircuit.slice(-1)[0];
+    return [evaluatedCircuit.slice(-1)[0], evaluatedCircuit];
 }
